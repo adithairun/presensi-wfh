ae2d881f8021bb6f542a7dd1f0c97600a1230201
Aditya Nugraha <73455725+adithairun@users.noreply.github.com>
2021-02-13 15:36:36 +0800

    edit save file

--- save_file.php
+++ save_file.php
@@ -24,7 +24,7 @@ $nama = mysqli_real_escape_string($conn, $nama);
 }
 		if(move_uploaded_file($file_temp, $location)){
 			mysqli_query($conn, "INSERT INTO `storage` VALUES('', '$file_name', '$file_type','$date', '$jam','$stud_no','$nama','$absen')") or die(mysqli_error($conn));
-			echo "<script>alert('Berhasil Simpan Presensi')</script>";		echo "<script>window.location = 'student_profile.php'</script>";
+			echo "<script>alert('Berhasil Simpan Presensi')</script>";		echo "<script>window.location = 'home.php'</script>";
 		}
 	}
 ?>
