b1702eb1b8bb13a7792dabe5d65086582fc4f3b2
Aditya Nugraha <adityanugraha795@gmail.com>
2021-02-16 21:26:17 +0800

    tambah menu hapus pengguna

--- admin/edit-data.php
+++ admin/edit-data.php
@@ -211,7 +211,7 @@
 			
             <div class="card-body">
 			<a  href="reset_pengguna.php"  onclick="return confirm('Seluruh Daftar Siswa Akan Dihapus')"><button class="btn btn-danger"> <span class="ion-aperture" ></span> Format Data Pengguna</button></a>
-			<button class="btn btn-success" data-toggle="modal" data-target="#form_modal"><span class="glyphicon glyphicon-plus"></span> Add Student</button>
+			
 			  <table id="example1" class="table table-bordered table-striped">
                 <thead>
                 <tr>
@@ -249,7 +249,9 @@
 						<td><center>
 
 						<button class="btn btn-success" data-toggle="modal" data-target="#edit_modal<?php echo $fetch['stud_id']?>"><span class="ion-compose"></span> Edit</button>
-<a  href="hapus_pengguna.php"  onclick="return confirm('Siswa Akan Dihapus, file yang diupload tidak akan dihapus')"><button class="btn btn-warning"> <span class="ion-trash-a" ></span> Hapus Data Pengguna</button></a>
+
+<a href="hapus_pengguna.php?stud_id=<?php echo $fetch['stud_id'];?>" onclick="return confirm('Data Siswa Akan Dihapus')"><button class="btn btn-warning"> <span class="" ></span> Hapus Pengguna</button></a>
+	
 						<button class="btn btn-danger btn-delete" id="<?php echo $fetch['stud_id']?>" type="button"><span class="ion-trash-b"></span> Hapus Seluruh Data</button></center>
 	
 <!--						 <button class="btn btn-success btn-lg active" role="button" data-toggle="modal" data-target="#exampleModal" aria-pressed="true">Bantuan</button> -->
@@ -283,69 +285,7 @@
  }
 
  ?>
- <div class="modal fade" id="form_modal" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered">
-			<div class="modal-content">
-				<form method="POST" action="save_student.php">	
-					<div class="modal-header">
-						<h4 class="modal-title">Add Student</h4>
-					</div>
-					<div class="modal-body">
-						<div class="col-md-3"></div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label>Student no</label>
-								<input oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" type="number" maxlength="5" name="stud_no" class="form-control" required="required"/>
-							</div>
-							<div class="form-group">
-								<label>Firstname</label>
-								<input type="text" name="firstname" class="form-control" required="required"/>
-							</div>
-							<div class="form-group">
-								<label>Lastname</label>
-								<input type="text" name="lastname" class="form-control" required="required"/>
-							</div>
-							<div class="form-group">
-								<label>Gender</label>
-								<select name="gender" class="form-control" required="required">
-									<option value=""></option>
-									<option value="Male">Male</option>
-									<option value="Female">Female</option>
-								</select>
-							</div>
-							<div class="form-inline">
-								<label>Year</label>
-								<select name="year" class="form-control" required="required">
-									<option value=""></option>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-									<option value="4">4</option>
-								</select>
-								<label>Section</label>
-								<select name="section" class="form-control" required="required">
-									<option value=""></option>
-									<option value="A">A</option>
-									<option value="B">B</option>
-									<option value="C">C</option>
-									<option value="D">D</option>
-								</select>
-							</div>
-							<br />
-							<div class="form-group">
-								<label>Password</label>
-								<input type="password" name="password" class="form-control" required="required"/>
-							</div>
-						</div>
-					</div>
-					<div style="clear:both;"></div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-danger" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Close</button>
-						<button name="save" class="btn btn-success" ><span class="glyphicon glyphicon-save"></span> Save</button>
-					</div>
-				</form>
-			</div>
-		</div>
+ 
 <!-- DISBALE PERINGATAN DATABLE -->
 
 					<script type="text/javascript">
---
+++ admin/hapus_pengguna.php
@@ -0,0 +1,14 @@
+<?php
+// koneksi database
+include '../akses/conn.php';
+ 
+// menangkap data id yang di kirim dari url
+$stud_id = $_GET['stud_id'];
+ 
+ 
+// menghapus data dari database
+mysqli_query($conn,"delete from student where stud_id='$stud_id'");
+ 
+// mengalihkan halaman kembali ke index.php
+echo "<script>alert('Berhasil Hapus Pengguna')</script>";		echo "<script>window.location = 'edit-data.php'</script>";
+ ?>
