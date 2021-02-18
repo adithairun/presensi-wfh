69c53f3d689b2e0e3c9f579e1c67de54f9b9b552
Aditya Nugraha <adityanugraha795@gmail.com>
2021-02-18 21:15:33 +0800

    edit save_file

--- save_file.php
+++ save_file.php
@@ -1,4 +1,3 @@
-
 <?php
 
 	require_once 'akses/conn.php';
@@ -8,12 +7,14 @@
 		$file_name = $_FILES['file']['name'];
 		$file_type = $_FILES['file']['type'];
 		$file_temp = $_FILES['file']['tmp_name'];
-		$location = "files/".$stud_no."/".$file_name;
 		$date = (date('Y-m-d'));
 		$jam = (date('H:i:s'));
 		
-		$nama = $_POST['nama'];
-		$absen = $_POST['absen'];
+		$ext	= pathinfo($file_name, PATHINFO_EXTENSION); 
+		$new_name  = (date('Y-m-d-h-i-s')) . '-' .$nama . '-' .$absen . '.' . $ext;
+		$location = "files/".$stud_no."/".$new_name ;
+		
+		
 		
 		if(!file_exists("files/".$stud_no)){
 			mkdir("files/".$stud_no);
@@ -23,7 +24,7 @@ $nama = mysqli_real_escape_string($conn, $nama);
 	
 }
 		if(move_uploaded_file($file_temp, $location)){
-			mysqli_query($conn, "INSERT INTO `storage` VALUES('', '$file_name', '$file_type','$date', '$jam','$stud_no','$nama','$absen')") or die(mysqli_error($conn));
+			mysqli_query($conn, "INSERT INTO `storage` VALUES('', '$new_name', '$file_type','$date', '$jam','$stud_no','$nama','$absen')") or die(mysqli_error($conn));
 			echo "<script>alert('Berhasil Simpan Presensi')</script>";		echo "<script>window.location = 'home'</script>";
 		}
 	}
